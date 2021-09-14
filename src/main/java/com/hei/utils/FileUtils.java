d2460e6cc9b33d1f4d96287336e1bfe623e5dda6
crystral <1023712219@qq.com>
2021-09-14 14:52:34 +0800

    功能一、完成txt文件的读取。

--- src/main/java/com/hei/utils/FileUtils.java
+++ src/main/java/com/hei/utils/FileUtils.java
@@ -1,5 +1,7 @@
 package com.hei.utils;
 
+import java.io.*;
+
 /**
  * TODO
  *
@@ -7,4 +9,30 @@ package com.hei.utils;
  * @date 2021/9/1414:05
  */
 public class FileUtils {
+    /**
+     * 读取txt文件
+     * @param filePath 读取文件路径、
+     * @return 将txt文件以字符串形式返回
+     */
+    public String readFile(String filePath){
+        File file=new File(filePath);
+        StringBuilder stringBuilder=new StringBuilder();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"UTF-8");
+            while (inputStreamReader.ready()){
+                stringBuilder.append((char) inputStreamReader.read());
+            }
+            //关闭读取流
+            inputStreamReader.close();
+            //关闭输入流，释放资源
+            fileInputStream.close();
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+
+
 }
